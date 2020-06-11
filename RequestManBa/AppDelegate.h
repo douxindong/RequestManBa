1b29eb4e8eb9afe9e5af9567c15b9c7594defd6b
xindong dou <douxd@lifevc.com>
2020-06-11 15:56:26 +0800

    Initial Commit

---
+++ RequestManBa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RequestManBa
+//
+//  Created by lifevc on 2020/6/11.
+//  Copyright © 2020 maba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RequestManBa/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RequestManBa
+//
+//  Created by lifevc on 2020/6/11.
+//  Copyright © 2020 maba. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
